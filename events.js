252a35b1707e8fb1ebf8b08222bcf23208ba8311
TonyKong0 <139964150+TonyKong0@users.noreply.github.com>
2025-09-16 13:07:10 +0800

    Add files via upload

---
+++ events.js
@@ -0,0 +1,146 @@
+import helpers from './helpers.js';
+
+window.addEventListener( 'load', () => {
+    //When the chat icon is clicked
+    document.querySelector( '#toggle-chat-pane' ).addEventListener( 'click', ( e ) => {
+        let chatElem = document.querySelector( '#chat-pane' );
+        let mainSecElem = document.querySelector( '#main-section' );
+
+        if ( chatElem.classList.contains( 'chat-opened' ) ) {
+            chatElem.setAttribute( 'hidden', true );
+            mainSecElem.classList.remove( 'col-md-9' );
+            mainSecElem.classList.add( 'col-md-12' );
+            chatElem.classList.remove( 'chat-opened' );
+        }
+
+        else {
+            chatElem.attributes.removeNamedItem( 'hidden' );
+            mainSecElem.classList.remove( 'col-md-12' );
+            mainSecElem.classList.add( 'col-md-9' );
+            chatElem.classList.add( 'chat-opened' );
+        }
+
+        //remove the 'New' badge on chat icon (if any) once chat is opened.
+        setTimeout( () => {
+            if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
+                helpers.toggleChatNotificationBadge();
+            }
+        }, 300 );
+    } );
+
+    document.getElementById("toggle-video").addEventListener("click", function() {
+        var videoStatusText = document.getElementsByClassName("status-text")[0];
+        if (videoStatusText.textContent === "关闭视频") {
+            videoStatusText.textContent = "开启视频";
+        } else {
+            videoStatusText.textContent = "关闭视频";
+        }
+    });
+    document.getElementById("toggle-mute").addEventListener("click", function() {
+        var muteStatusText = document.getElementsByClassName("status-text")[1];
+        if (muteStatusText.textContent === "闭麦") {
+            muteStatusText.textContent = "开麦";
+        } else {
+            muteStatusText.textContent = "闭麦";
+        }
+    });
+    //When the video frame is clicked. This will enable picture-in-picture
+    document.getElementById( 'local' ).addEventListener( 'click', () => {
+        if ( !document.pictureInPictureElement ) {
+            document.getElementById( 'local' ).requestPictureInPicture()
+                .catch( error => {
+                    // Video failed to enter Picture-in-Picture mode.
+                    console.error( error );
+                } );
+        }
+
+        else {
+            document.exitPictureInPicture()
+                .catch( error => {
+                    // Video failed to leave Picture-in-Picture mode.
+                    console.error( error );
+                } );
+        }
+    } );
+
+
+    //When the 'Create room" is button is clicked
+    document.getElementById( 'create-room' ).addEventListener( 'click', ( e ) => {
+        e.preventDefault();
+
+        let roomName = document.querySelector( '#room-name' ).value;
+        let yourName = document.querySelector( '#your-name' ).value;
+
+        if ( roomName && yourName ) {
+            //remove error message, if any
+            document.querySelector('#err-msg').innerText = "";
+
+            //save the user's name in sessionStorage
+            sessionStorage.setItem( 'username', yourName );
+
+            //create room link
+            let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
+
+            //show message with link to room
+            document.querySelector('#room-created').innerHTML = `<center>房间创建成功，点击 <a href='${ roomLink }'>这里</a> 进入房间吧！<br>分享链接可邀请同伴进入房间～</center>`;
+
+            //empty the values
+            document.querySelector( '#room-name' ).value = '';
+            document.querySelector( '#your-name' ).value = '';
+        }
+
+        else {
+            document.querySelector('#err-msg').innerText = "请填写所有空格";
+        }
+    } );
+
+
+    //When the 'Enter room' button is clicked.
+    document.getElementById( 'enter-room' ).addEventListener( 'click', ( e ) => {
+        e.preventDefault();
+
+        let name = document.querySelector( '#username' ).value;
+
+        if ( name ) {
+            //remove error message, if any
+            document.querySelector('#err-msg-username').innerText = "";
+
+            //save the user's name in sessionStorage
+            sessionStorage.setItem( 'username', name );
+
+            //reload room
+            location.reload();
+        }
+
+        else {
+            document.querySelector('#err-msg-username').innerText = "请输入您的昵称";
+        }
+    } );
+
+
+    document.addEventListener( 'click', ( e ) => {
+        if ( e.target && e.target.classList.contains( 'expand-remote-video' ) ) {
+            helpers.maximiseStream( e );
+        }
+
+        else if ( e.target && e.target.classList.contains( 'mute-remote-mic' ) ) {
+            helpers.singleStreamToggleMute( e );
+        }
+    } );
+
+
+    document.getElementById( 'closeModal' ).addEventListener( 'click', () => {
+        helpers.toggleModal( 'recording-options-modal', false );
+    } );
+
+    // 录制选项卡片点击事件
+    document.getElementById( 'record-video' ).addEventListener( 'click', () => {
+        // 触发视频录制
+        document.getElementById( 'record-video' ).click();
+    } );
+
+    document.getElementById( 'record-screen' ).addEventListener( 'click', () => {
+        // 触发屏幕录制
+        document.getElementById( 'record-screen' ).click();
+    } );
+} );
